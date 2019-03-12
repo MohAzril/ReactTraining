f0806f4b101288292494c4fff7b88cfab9630098
MohAzril <azril@alphatech.id>
2019-03-12 16:09:34 +0700

    list news and top news

--- src/components/SideList.js
+++ src/components/SideList.js
@@ -3,30 +3,16 @@ import React, { Component } from 'react';
 // import '../styles/style.css';
 import '../styles/blog.css';
 
-class SideList extends Component {
-  render() {
-    return (
-      <div className="SideList">
-        <ul class="list-group">
-          <li class="list-group-item d-flex justify-content-between align-items-center fav">
-            Artikel Favorit
-          </li>
-          <li class="list-group-item d-flex justify-content-between align-items-center">
-            Artikel 1
-            <span class="badge badge-primary badge-pill">1</span>
-          </li>
-          <li class="list-group-item d-flex justify-content-between align-items-center">
-            Artikel 2
-            <span class="badge badge-primary badge-pill">2</span>
-          </li>
-          <li class="list-group-item d-flex justify-content-between align-items-center">
-            Artikel 3
-            <span class="badge badge-primary badge-pill">3</span>
-          </li>
-        </ul>
-      </div>
-    );
-  }
+const SideList =props => {  
+  return (
+    <div className="top-rated">
+      <li class="list-group-item d-flex justify-content-between align-items-center">
+      {props.title}
+        <span class="badge badge-primary badge-pill">{props.index+1}</span>
+      </li>
+    </div>
+  );
 }
+
 
 export default SideList;
--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -3,9 +3,57 @@ import React, { Component } from 'react';
 import '../styles/blog.css';
 import Search from '../components/Search.js'
 import SideList from '../components/SideList.js'
+import ListNews from '../components/ListNews'
+import axios from 'axios';
+
+//dummy date
+import az from "../images/berita1.jpg"
+
+//News API
+const apiKey = "72aadd1aff8c490ea5c90d2e5225a042";
+const baseUrl = "https://newsapi.org/v2/"
+const urlHeadline = baseUrl + "top-headlines?" + "country=id&" + "pageSize=3&"+ "apiKey=" + apiKey;
+const urlNews = baseUrl + "everything?" +"q=meme&" + "pageSize=3&"+ "apiKey=" + apiKey;
 
 class Blog extends Component {
-  render() {
+constructor(props){
+    super(props);
+    this.state = {
+        listNews:[],
+        listTopNews:[],
+        username:"",
+        isLogin:false
+    };
+}
+componentDidMount = () =>{
+    const self = this;
+    axios.get(urlNews)
+    .then(function(response){
+        self.setState({listNews:response.data.articles });
+        // handle response
+        console.log(response.data);
+    })
+    .catch(function(error){
+        // handle error
+        console.log(error);
+    });
+    axios.get(urlHeadline)
+    .then(function(response){
+        self.setState({listTopNews:response.data.articles });
+        // handle response
+        console.log(response.data);
+    })
+    .catch(function(error){
+        // handle error
+        console.log(error);
+    });
+} 
+  
+render() {
+    console.log("here")
+    // const news = this.state.ListNews;
+    const {listNews, listTopNews, username, isLogin} = this.state;
+    
     return (
     <div className="Blog">
         
@@ -26,12 +74,29 @@ class Blog extends Component {
                     <a href="#">Start Bootstrap</a>
                 </div>    
             </div>    
+            {listNews.map((item,key) =>{
+                const src_img = item.urlToImage === null ? az : item.urlToImage;
+                const content = item.urlToImage !== null ? item.content : "";
+                return <ListNews key={key} title={item.title} img={src_img} content={content}/>;
+            })}
         </div>
 
         
         <div class="col-md-4">
             <Search/>
-            <SideList/>
+            <div className="SideList">
+                <ul class="list-group">
+                <li class="list-group-item d-flex justify-content-between align-items-center fav">
+                    Artikel Favorit
+                </li>
+                {listTopNews.map((item,key) =>{
+                const src_img = item.urlToImage === null ? az : item.urlToImage;
+                const content = item.urlToImage !== null ? item.content : "";
+                return <SideList index={key} title={item.title} img={src_img} content={content}/>;
+                })}
+                </ul>
+            </div>
+            {/* <SideList/> */}
         </div>
         
         </div>
